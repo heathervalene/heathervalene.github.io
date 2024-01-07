78e3106cb2ecda406571251a97cf225f4ccbf276
HeatherRouke <heather214@gmail.com>
2024-01-07 17:18:26 -0500

    fix style

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
-document.querySelectorAll('a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        const targetPage = this.getAttribute('href');
-        history.pushState(null, null, targetPage);
-    });
-});
 
