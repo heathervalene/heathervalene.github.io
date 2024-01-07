5fb79890a44b6f46c9abea14f3fe88c512c71c2f
HeatherRouke <heather214@gmail.com>
2024-01-07 17:16:49 -0500

    link fix

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
+document.querySelectorAll('a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
 
+        const targetPage = this.getAttribute('href');
+        history.pushState(null, null, targetPage);
+    });
+});
 
