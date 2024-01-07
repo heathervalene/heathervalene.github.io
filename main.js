ee8081c01f28cedc58650f02be664af98773b430
HeatherRouke <heather214@gmail.com>
2024-01-07 14:48:38 -0500

    layout changes

--- main.js
+++ main.js
@@ -1,33 +1,5 @@
 
-const projectOne = 'images/MemoryGame.png';
 
 
-/*----- cached elements -----*/
-
-
-
-
-
-
-/*----- event listeners -----*/
-
-
-
-
-
-/*----- functions -----*/
-
-
-function handleCardClick(evt) {
-    const clickedCard = evt.currentTarget;
-    flipCard(clickedCard);
-   
-}
-
-
-//function to flip the individual project card when clicked
-const flipCard = () => {
-cardInner.classList.toggle('flipped');
-}
 
 
